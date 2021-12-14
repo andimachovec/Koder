bbd9e8ba0849c51839006b60ea768540eba75ca3
Andi Machovec <andi.machovec@gmail.com>
2021-12-14 17:25:43 +0100

    removed dummy menuitems used for debugging

--- src/controls/FunctionView.cpp
+++ src/controls/FunctionView.cpp
@@ -16,8 +16,6 @@ FunctionView::FunctionView()
 {
 
 	fPopUpMenu = new BPopUpMenu("");
-	fPopUpMenu->AddItem(new BMenuItem("Class1::Function1", new BMessage(FV_SELECTION_CHANGED)));
-	fPopUpMenu->AddItem(new BMenuItem("Class1::Function2", new BMessage(FV_SELECTION_CHANGED)));
 	fMenuField = new BMenuField("",fPopUpMenu);
 
 	BLayoutBuilder::Group<>(this, B_VERTICAL, B_USE_SMALL_SPACING)
